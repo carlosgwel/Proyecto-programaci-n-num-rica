6228e448f7aa644b4147cce099e9aae404ff41de
carlosgwel <lax8324@gmail.com>
2025-05-23 10:59:02 -0600

    Add files via upload

--- interpolacion cuadratica.py
+++ interpolacion cuadratica.py
@@ -1,29 +1,17 @@
-x = [1, 2, 3, 5, 7]
-y = [3, 6, 19, 99, 291]
+import matplotlib.pyplot as plt
+x = [2, 3, 5]
+y = [6, 19, 99]
 
-n = len(x)
-dif = [[0]*n for _ in range(n)]
-for i in range(n):
-    dif[i][0] = y[i]
-
-for j in range(1,n):
-    for i in range(n-j):
-        dif[i][j] = (dif[i+1][j-1]-dif[i][j-1])/(x[i+j]-x[i])
-
-coef = [dif[0][i] for i in range(n)]
-
+b0 = y[0]
+b1 = (y[1] - y[0]) / (x[1] - x[0])
+b2 = ((y[2] - y[1]) / (x[2] - x[1]) - b1) / (x[2] - x[0])
 def P(x_val):
-    result = coef[-1]
-    for i in range(n-2,-1,-1):
-        result = result*(x_val - x[i]) + coef[i]
-    return result
-
+    return b0 + b1 * (x_val - x[0]) + b2 * (x_val - x[0]) * (x_val - x[1])
 f4 = P(4)
-print(f"f(4) = {f4:.2f}")
+print(f"Estimación de f(4) por interpolación cuadrática: {f4:.2f}")
 
-x_curve = [x[0] + i*(x[-1]-x[0])/100 for i in range(101)]
+x_curve = [x[0] + i * (x[-1] - x[0]) / 100 for i in range(101)]
 y_curve = [P(xi) for xi in x_curve]
 
-import matplotlib.pyplot as plt
-plt.scatter(x, y, color='red')
-plt.plot(x_curve, y_curve, 'b-')
+plt.scatter(x, y, color='red', label='Puntos dados')
+plt.plot(x_curve, y_curve, 'b-', label='Curva de interpolación cuadrática')
